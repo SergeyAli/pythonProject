8a46099f05cbe283f625fce3dadd5ec20e7f4285
Sergey <aliger99@rambler.ru>
2023-07-16 18:01:36 +0700

    Семинар_5_Домашка_Задача_2

--- PythonGB/Seminar/05/2_home_task.py
+++ PythonGB/Seminar/05/2_home_task.py
@@ -4,3 +4,14 @@
 и суммой премии в качестве значения. Сумма рассчитывается как ставка умноженная на процент премии
 '''
 
+
+names = ['Сергей', 'Михаил', 'Светлана', 'Ян']
+salary = [55000, 42000, 38000, 28000]
+bonus = ['10.25%', '15.00%', '18.50%', '12.05%']
+
+
+def salary_gen(names: list[str], salary: list[int], bonus: list[str]) -> dict[str: float]:
+    return {name: sale / 100 * bon for name, sale, bon in zip(names, salary, (float(i[:-1]) for i in bonus))}.items()
+
+
+print(*(salary_gen(names, salary, bonus)))
