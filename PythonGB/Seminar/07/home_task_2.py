c0e337bfbdd58a914c79f5822e57d7d21d67e68a
Sergey <aliger99@rambler.ru>
2023-07-24 20:19:14 +0700

    Семинар_7_Домашка_0

--- PythonGB/Seminar/07/home_task_2.py
+++ PythonGB/Seminar/07/home_task_2.py
@@ -1,7 +1,7 @@
 '''
 Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
 '''
-
+import os
 
 from task_1 import add_number
 from task_2 import gen_psevdonim
@@ -9,6 +9,7 @@ from task_3 import create_file
 from TASK_04.task_4 import create_file_
 from TASK_04.task_5 import create_dif_files
 from TASK_04.task_6 import create_dir
+from TASK_07.task_7 import group
 
 if __name__ == '__main__':
     add_number(5, 'task01.txt')
@@ -17,4 +18,4 @@ if __name__ == '__main__':
     create_file_('txt', count_file=3)
     create_dif_files(txt=2, bin=4, png=8)
     create_dir('new')
-
+    group(os.getcwd())
