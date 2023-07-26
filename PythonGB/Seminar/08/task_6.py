756a35c4fa1715d38aa5ab67358da59d06d3fc24
Sergey <aliger99@rambler.ru>
2023-07-26 22:22:36 +0700

    Семинар_8_0

--- PythonGB/Seminar/08/task_6.py
+++ PythonGB/Seminar/08/task_6.py
@@ -5,4 +5,37 @@
 📌 Функция должна извлекать ключи словаря для заголовков столбца из переданного файла
 '''
 
+import csv
+import pickle
+from pathlib import Path
 
+
+# from typing import Dict
+
+
+def read_pickle(fail: Path) -> list[dict]:
+    with open(fail, 'rb') as f_pkl:
+        data_lst = pickle.load(f_pkl, encoding='utf-8')
+    print(data_lst)
+    return data_lst
+
+
+def form_lst(data_lst: list[dict]) -> list[list[str]]:
+    out_lst = [[i_key for i_key in data_lst[0].keys()]]
+    for dct in data_lst:
+        out_lst.append([*dct.values()])
+    return out_lst
+
+
+def write_csv(pkl_file: list[list[str]], file_out: Path) -> None:
+    with open(file_out, 'w', newline='', encoding='utf-8') as f:
+        csv_writer = csv.writer(f, dialect='excel')
+        csv_writer.writerows(pkl_file)
+
+
+def pickle_to_csv(fail_in: Path, fail_out: Path) -> None:
+    write_csv(form_lst(read_pickle(fail_in)), fail_out)
+
+
+if __name__ == '__main__':
+    pickle_to_csv(Path('json_pickle.bin'), Path('pickle_to_csv.csv'))
--- PythonGB/Seminar/08/task_7.py
+++ PythonGB/Seminar/08/task_7.py
@@ -4,4 +4,22 @@
 📌 Распечатайте его как pickle строку
 '''
 
+import pickle
+from pathlib import Path
 
+
+def reader_csv(file: Path) -> str:
+    with open(file, 'r', encoding='utf-8') as f_csv:
+        return f_csv.read()
+
+
+def print_pickle(file_csv: str) -> None:
+    print(pickle.dumps(file_csv))
+
+
+def csv_to_pickle(file_csv: Path):
+    print_pickle(reader_csv(file_csv))
+
+
+if __name__ == '__main__':
+    csv_to_pickle(Path('pickle_to_csv.csv'))
