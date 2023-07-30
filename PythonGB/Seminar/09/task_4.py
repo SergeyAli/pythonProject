52d8180a0dd83b1112d9d684a843c84f4ebeaac3
Sergey <aliger99@rambler.ru>
2023-07-30 12:03:12 +0700

    Семинар_9_0

--- PythonGB/Seminar/09/task_4.py
+++ PythonGB/Seminar/09/task_4.py
@@ -4,4 +4,23 @@
 📌 Параметр - целое число, количество запусков декорируемой функции.
 '''
 
+import random
+from functools import wraps
 
+def run_multiple_times(num_runs):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            results = []
+            for _ in range(num_runs):
+                results.append(func(*args, **kwargs))
+            return results
+        return wrapper
+    return decorator
+
+if __name__ == '__main__':
+    @run_multiple_times(5)
+    def generate_random_number():
+        return random.randint(1, 10)
+
+    print(generate_random_number())
